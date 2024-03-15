b3848f7b95a41cb9e6f872f8cf232cd04580749a
pavankumar19992208 <pavankumar19992208@gmail.com>
2024-03-16 03:56:32 +0530

    home

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -38,7 +38,7 @@ class Header extends Component {
             <ul className="menu" style={{ backgroundColor: theme.body }}>
               <li>
                 <NavLink
-                  to="/home"
+                  to="/"
                   tag={Link}
                   activeStyle={{ fontWeight: "bold" }}
                   style={{ color: theme.text }}
